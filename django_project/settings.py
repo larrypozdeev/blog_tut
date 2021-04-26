f3e8d39c698c91c1f8dcbe342b8af71c419887c4
Larry Pozdeev <dmitry_pozdeev@icloud.com>
2021-04-27 01:14:51 +0300

    hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['127.0.0.1','myadorableblog.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition

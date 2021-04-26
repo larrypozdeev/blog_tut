83b3437aa0e87f77ed10373a1a027dfae3957b8d
Larry Pozdeev <dmitry_pozdeev@icloud.com>
2021-04-26 22:04:37 +0300

    Updated SECRET_KEY

--- django_project/settings.py
+++ django_project/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p2u)^7)#&gnr-=(&fdt$slau#=9)@c(ba9r*&@5cv(7dnt)^kd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['myadorableblog.herokuapp.com']
 
